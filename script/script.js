2388e4bd283179cf3d7a8d5b43ea7684bb6f95e0
Julia Khayrulina <julia748@mail.ru>
2021-09-17 23:32:46 +0300

    Rewrite to ES6+

--- script/script.js
+++ script/script.js
@@ -1,1 +1,40 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+  //==========таймер==============//
+
+  const countTimer = (deadline) => {
+    let timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
+
+    const getTimeRemaining = () => {
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000, //получаем разницу в мс / 1000 => в секунды
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+
+      return { dateStop, dateNow, hours, minutes, seconds };
+    };
+
+    const upDateClock = () => {
+      let timer = getTimeRemaining();
+
+      timerHours.textContent = ('0' + timer.hours).slice(-2);
+      timerMinutes.textContent = ('0' + timer.minutes).slice(-2);
+      timerSeconds.textContent = ('0' + timer.seconds).slice(-2);
+
+      if (timer.dateStop < timer.dateNow) {
+        clearInterval(idSetInterval);
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+      }
+    };
+    upDateClock();
+  };
+  let idSetInterval = setInterval(countTimer, 1000, '12 sept 2021');
+  countTimer('12 sept 2021');
+});
